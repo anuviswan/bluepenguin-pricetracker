3975edaa2df53b58f94f67e8c9a18e9672ce9967
Anu Viswan <anu.viswan@gmail.com>
2025-09-16 20:28:40 +0530

    adding apisettings

---
+++ BP.PriceTracker.Services/Options/ApiSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP.PriceTracker.Services.Options;
+
+public record ApiSettings
+{
+    public string BaseUrl { get; init; } = string.Empty;
+    public string LoginEndpoint { get; init; } = string.Empty;
+}
