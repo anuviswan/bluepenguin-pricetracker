28df40af072529a111a105860c33f3fa38c3d3f7
Anu Viswan <anu.viswan@gmail.com>
2026-03-15 14:07:53 +0530

    added ITokenService moved CameraService to MAUI project

--- BP.PriceTracker.Services/Services/ImageSearchService.cs
+++ BP.PriceTracker.Services/Services/ImageSearchService.cs
@@ -2,17 +2,16 @@
 using BP.PriceTracker.Services.Options;
 using BP.PriceTracker.Services.Types;
 using Microsoft.Extensions.Options;
-using Microsoft.Maui.Storage;
 
 namespace BP.PriceTracker.Services.Services;
 
-public class ImageSearchService(IApiService apiService, IOptions<ApiSettings> apiOptions) : IImageSearchService
+public class ImageSearchService(IApiService apiService, IOptions<ApiSettings> apiOptions, ITokenService token) : IImageSearchService
 {
     private ApiSettings ApiSettings => apiOptions.Value;
     public async Task<ImageSearchResultResponse?> SearchByImage(Stream stream)
     {
         var endpoint = ApiSettings.SearchByImageEndpoint;
-        var authToken = await SecureStorage.GetAsync("auth_token");
+        var authToken = await token.GetAuthTokenAsync();
         var response = await apiService.PostAsync<ImageSearchResultResponse>(endpoint, stream, "image.jpg",authToken).ConfigureAwait(false);
         return response.IsSuccess ? response.Data! : null;
     }
